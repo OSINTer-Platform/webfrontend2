45b148a97f2c80c3ecacea7ce159fb43911ae4f6
bertmad3400 <skrivtilbertram@gmail.com>
2023-02-15 18:10:24 +0100

    Add initial classes for handling objects from backend

--- src/lib/shared/types.ts
+++ src/lib/shared/types.ts
@@ -25,4 +25,41 @@ export interface Article extends ArticleBase {
         cluster: number;
     };
 }
+
+export interface ItemBase {
+    id: string;
+    name: string;
+    owner: string;
+}
+
+export interface Collection extends ItemBase {
+    ids: string[];
+}
+
+enum SortBy {
+    publish_date,
+    read_times,
+    source,
+    author,
+    inserted_at,
+}
+
+enum SortOrder {
+    desc,
+    asc,
+}
+
+export interface Feed extends ItemBase {
+    limit: number;
+
+    sort_by: SortBy | null;
+    sort_order: SortOrder | null;
+
+    search_term: string | null;
+    highlight: boolean;
+
+    first_date: string;
+    last_date: string;
+
+    source_category: string[];
 }
