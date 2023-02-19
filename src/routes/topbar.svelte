5413514d9f8937448e8cbcf6d0e51977fc4f1c02
bertmad3400 <skrivtilbertram@gmail.com>
2023-02-19 11:44:01 +0100

    Fix styling of topbar

--- src/routes/topbar.svelte
+++ src/routes/topbar.svelte
@@ -28,9 +28,23 @@
     ];
 </script>
 
-	<div class="flex items-center space-x-4">
+<header class="
+	w-full
+
+	flex
+	flex-row
+	justify-between
+
+	bg-surface-100
+	dark:bg-surface-800
+
+	shadow-lg
+
+	p-6
+">
+	<div class="flex items-center gap-8">
 		<!-- Hamburger Menu -->
-		<button on:click={} class="btn-icon btn-icon-sm lg:!hidden">
+		<button on:click={() => {}} class="btn lg:!hidden p-2 rounded-xl">
 			<Fa icon={faBars} class="text-xl" />
 		</button>
 
@@ -44,45 +58,95 @@
 		</a>
 	</div>
 
-	<!-- Navigate -->
-	<div class="relative hidden lg:block">
-		<button
-			class="btn hover:variant-soft-primary"
-		>
-			<span>Navigate</span>
-			<Fa icon={faCaretDown} />
-		</button>
-		<div class="card w-60 py-1 shadow-xl" data-menu="features">
-			<nav class="list-nav divide-y divide-tertiary-500">
-				{#each Object.values(navItems) as items}
-					<ul class="p-2">
-						{#each items as item}
-							<li>
-								<a href={item.route}>
-									<span class="w-6 text-center"
-										><Fa icon={item.icon} /></span
-									>
-									<span>{item.text}</span>
-								</a>
-							</li>
-						{/each}
-					</ul>
-				{/each}
-			</nav>
-		</div>
-	</div>
+	<section class="
+		flex
+		flex-row
 
-	<!-- Social -->
-	<section class="flex space-x-2">
-		{#each socials as social}
-			<a
-				class="btn-icon hover:variant-soft-primary"
-				title={social.text}
-				href={social.route}
-				target="_blank"
-				rel="noreferrer noopener"
-			>
-				<Fa icon={social.icon} class="text-2xl" />
-			</a>
-		{/each}
+		gap-10
+	">
+		<!-- Navigate -->
+		<div class="
+			relative
+
+			hidden
+			lg:block
+
+			[&:focus-within>div]:block
+		">
+			<button class="
+				btn
+
+				py-2
+				px-6
+
+				rounded-xl
+			">
+				Navigate
+				<Fa icon={faCaretDown} />
+			</button>
+			<div class="
+				hidden
+				absolute
+
+				top-12
+				right-0
+
+				bg-surface-100
+				rounded-xl
+
+				border-tertiary-500
+				border
+
+				w-64
+
+				shadow-xl
+			">
+				<nav class="divide-y divide-tertiary-500 py-2 px-4">
+					{#each Object.values(navItems) as items}
+						<ul class="py-3">
+							{#each items as item}
+								<li class="w-full text-left">
+									<a href={item.route} class="
+										flex
+										flex-row
+										gap-6
+
+										hover:bg-primary-300/25
+
+										items-center
+										justify-start
+
+										rounded-xl
+
+										w-full
+										p-3
+										pl-5
+									">
+										<Fa icon={item.icon} />
+										{item.text}
+									</a>
+								</li>
+							{/each}
+						</ul>
+					{/each}
+				</nav>
+			</div>
+		</div>
+
+		<!-- Social -->
+		<section class="flex items-center gap-2">
+			{#each socials as social}
+				<a
+					class="btn p-2 rounded-full"
+					title={social.text}
+					href={social.route}
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					<Fa icon={social.icon} class="text-2xl" />
+				</a>
+			{/each}
+		</section>
 	</section>
+
+</header>
