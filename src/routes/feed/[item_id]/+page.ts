25e7c0e1834f5bb67a2e1e4d3bb8a829b1920833
bertmad3400 <skrivtilbertram@gmail.com>
2023-02-21 15:46:33 +0100

    Fetch current user item

--- src/routes/feed/[item_id]/+page.ts
+++ src/routes/feed/[item_id]/+page.ts
@@ -1,18 +1,33 @@
 import type { PageLoad } from './$types';
 
 import { config } from '$shared/config';
-import type { ArticleBase } from '$shared/types';
+import type { ArticleBase, ItemBase } from '$shared/types';
+import { error } from '@sveltejs/kit';
 
 export const load = (({ params, fetch }) => {
+    const paramUrl = `${config.apiRoot}/user-items/${params.item_id}`;
+
+    const handleResponse = async (response: Response) => {
+        const json = await response.json();
+        if (response.ok) {
+            return json;
+        } else {
+            throw error(response.status, json);
+        }
+    };
+
     const fetchArticles = async (): Promise<ArticleBase[]> => {
-        const r = await fetch(
-            `${config.apiRoot}/user-items/${params.item_id}/articles`
-        );
-        const articles = await r.json();
-        return articles;
+        const r = await fetch(`${paramUrl}/articles`);
+        return handleResponse(r);
+    };
+
+    const fetchItem = async (): Promise<ItemBase> => {
+        const r = await fetch(`${paramUrl}/content`);
+        return handleResponse(r);
     };
 
     return {
         articles: fetchArticles(),
+        currentItem: fetchItem(),
     };
 }) satisfies PageLoad;
