ad50a26e834dd5ecfb7aa841ba7b81117db674b9
bertmad3400 <skrivtilbertram@gmail.com>
2023-02-27 13:49:44 +0100

    Add tab component for radio input groups

---
+++ src/lib/components/tabs.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    export let options: { [key: string]: string };
+
+    $: formName = Object.entries(options)
+        .map(([name, value]) => value)
+        .join('-');
+
+    export let selected: string;
+</script>
+
+<div class="flex gap-2">
+    {#each Object.entries(options) as [name, value]}
+        <label
+            class="
+			px-4 py-2
+			rounded-t-md
+
+			cursor-pointer
+
+			transition-colors
+			hover:bg-primary-400/20
+
+			border-primary-500
+
+		"
+            class:border-b-4={value == selected}
+        >
+            <input
+                type="radio"
+                bind:group={selected}
+                name={formName}
+                {value}
+                class="hidden"
+            />
+            {name}
+        </label>
+    {/each}
+</div>
