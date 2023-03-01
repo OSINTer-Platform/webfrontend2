a6ee0fa1d469ffb46c4b5ccb5edb7183fc5dc496
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-01 19:34:23 +0100

    Refactor general sidebar component

---
+++ src/lib/components/sidebar/linkList.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import type { SidebarOption } from '$shared/nav';
+
+    import AppSwitcher from './appSwitcher.svelte';
+
+    import LinkNavShell from './linkNav/shell.svelte';
+    import LinkNavOptions from './linkNav/optionList.svelte';
+
+    import SidebarShell from './shell.svelte';
+
+	export let options: Array<SidebarOption> = []
+</script>
+
+<SidebarShell>
+    <AppSwitcher />
+
+    {#if options.length > 0}
+        <LinkNavShell>
+            {#each options as option}
+                {#if option.list.length > 0}
+                    <LinkNavOptions options={option} />
+                {/if}
+            {/each}
+        </LinkNavShell>
+    {/if}
+</SidebarShell>
