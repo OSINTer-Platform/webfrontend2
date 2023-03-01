82d665c59ea391993971590c558354377091c081
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-01 10:48:16 +0100

    Add type for article render

--- src/lib/components/article-list/main.svelte
+++ src/lib/components/article-list/main.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
     import type { ArticleBase } from '$shared/types/api';
+    import type { ArticleRender } from '$shared/types/internal';
 
     import Large from './component/large.svelte';
     import Title from './component/title.svelte';
 
     export let articles: ArticleBase[] = [];
-    export let layout: string = 'large';
+    export let layout: ArticleRender = 'large';
 
-    const layouts: { [key: string]: any } = {
+    const layouts: { [articleRender in ArticleRender]: any } = {
         large: Large,
         title: Title,
     };
