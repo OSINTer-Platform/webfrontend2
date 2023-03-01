f10ea771a19e6482a322e5e70386d6cd88640556
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-01 22:25:45 +0100

    Cache articles when getting content

--- src/lib/shared/state.ts
+++ src/lib/shared/state.ts
@@ -1,7 +1,10 @@
 import { writable, type Writable } from 'svelte/store';
+import type { Article } from './types/api';
 import type { ArticleRender } from './types/internal';
 
 export const sideOpen: Writable<{ [key: string]: Boolean }> = writable({});
 
 export const localSearch: Writable<string> = writable('');
 export const articleRender: Writable<ArticleRender> = writable('large');
+
+export const fullArticles: Writable<{ [key: string]: Article }> = writable({});
--- src/routes/article/[article_id]/+page.ts
+++ src/routes/article/[article_id]/+page.ts
@@ -3,12 +3,27 @@ import type { PageLoad } from './$types';
 import { config } from '$shared/config';
 import type { Article } from '$shared/types/api';
 
+import { get } from 'svelte/store';
+import { fullArticles } from '$shared/state';
+
 export const load = (({ params, fetch }) => {
+    const fullArticleList = get(fullArticles);
+
     const fetchContent = async (): Promise<Article> => {
+        if (params.article_id in fullArticleList) {
+            return fullArticleList[params.article_id];
+        }
+
         const r = await fetch(
             `${config.apiRoot}/articles/${params.article_id}/content`
         );
         const article = await r.json();
+
+        fullArticles.update((list) => {
+            list[params.article_id] = article;
+            return list;
+        });
+
         return article;
     };
 
