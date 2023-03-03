74e8f65695b319ca4e541103ed81e5995082d533
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-03 11:06:19 +0100

    Add wrapper around article list to refactor

---
+++ src/lib/components/article-list/wrapper.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { articleListRender, localSearch } from '$shared/state';
+    import { filterArticles } from '$lib/common';
+    import type { ArticleBase } from '$shared/types/api';
+    import List from './main.svelte';
+
+    export let articles: ArticleBase[];
+</script>
+
+<div
+    class="
+	bg-surface-100
+
+	p-6
+	sm:p-12
+"
+>
+    <List
+        articles={filterArticles(articles, $localSearch)}
+        layout={$articleListRender}
+    />
+</div>
--- src/routes/feed/(list)/[feed_name=inbuilt]/+page.svelte
+++ src/routes/feed/(list)/[feed_name=inbuilt]/+page.svelte
@@ -1,23 +1,8 @@
 <script lang="ts">
     import type { PageData } from './$types';
-    import List from '$com/article-list/main.svelte';
-
-    import { articleListRender, localSearch } from '$shared/state';
-    import { filterArticles } from '$lib/common';
+    import List from '$com/article-list/wrapper.svelte';
 
     export let data: PageData;
 </script>
 
-<div
-    class="
-	bg-surface-100
-
-	p-6
-	sm:p-12
-"
->
-    <List
-        articles={filterArticles(data.articles, $localSearch)}
-        layout={$articleListRender}
-    />
-</div>
+<List articles={data.articles} />
