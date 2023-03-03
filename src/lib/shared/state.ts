783459f239c23f412ecc288409ad26a28836cdc1
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-03 10:35:14 +0100

    Use persistent stores that syncs to local storage for state management

--- src/lib/shared/state.ts
+++ src/lib/shared/state.ts
@@ -1,10 +1,21 @@
-import { writable, type Writable } from 'svelte/store';
+import { persisted } from 'svelte-local-storage-store';
+import type { Writable } from 'svelte/store';
+
 import type { Article } from './types/api';
 import type { ArticleListRender } from './types/internal';
 
-export const sideOpen: Writable<{ [key: string]: Boolean }> = writable({});
+export const sideOpen: Writable<{ [key: string]: Boolean }> = persisted(
+    'sidebar-open',
+    {}
+);
 
-export const localSearch: Writable<string> = writable('');
-export const articleListRender: Writable<ArticleListRender> = writable('large');
+export const localSearch: Writable<string> = persisted('localSearch', '');
+export const articleListRender: Writable<ArticleListRender> = persisted(
+    'articleListRender',
+    'large'
+);
 
-export const fullArticles: Writable<{ [key: string]: Article }> = writable({});
+export const fullArticles: Writable<{ [key: string]: Article }> = persisted(
+    'fullArticleList',
+    {}
+);
