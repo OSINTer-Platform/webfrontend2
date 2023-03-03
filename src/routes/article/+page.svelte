cac138dab7806f4a6fd8668b4e88ed85be58c2b1
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-03 12:23:12 +0100

    Add option to clear stored articles

--- src/routes/article/+page.svelte
+++ src/routes/article/+page.svelte
@@ -3,11 +3,22 @@
 
     import List from '$com/article-list/wrapper.svelte';
     import { fullArticles } from '$shared/state';
+    import type { HeaderModOptions } from '$shared/types/internal';
+    import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
+
+    const modOptions: Array<HeaderModOptions> = [
+		{
+			text: 'Clear stored',
+			icon: faTrashCan,
+			action: () => fullArticles.set({})
+		}
+	]
 </script>
 
 <HeaderShell
     title={'Stored articles'}
     description={'A list of the articles you have recently viewed in OSINTer'}
+	{modOptions}
 >
     <hr class="text-tertiary-600/50 sm:my-4" />
 </HeaderShell>
