89170268e0ccde734aedab8c93e72ee7769fbf63
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-06 16:43:21 +0100

    Create seperate class for search bars

--- src/lib/components/article-list/header/search.svelte
+++ src/lib/components/article-list/header/search.svelte
@@ -5,23 +5,8 @@
     export let articleSearch: string = '';
 </script>
 
-<div
-    class="
-	w-full
-	my-6
-
-	flex
-
-	border
-	border-tertiary-600
-
-	text-black
-
-	bg-surface-600/10
-	rounded-md
-"
->
-    <label for="search-field" class="m-2 text-tertiary-800">
+<div class="w-full my-6 search-field">
+    <label for="search-field">
         <Fa icon={faMagnifyingGlass} />
     </label>
 
@@ -29,20 +14,5 @@
         bind:value={articleSearch}
         id="search-field"
         placeholder="Search in article content"
-        class="
-		w-full
-
-		bg-transparent
-		placeholder-tertiary-800
-
-		p-2
-
-		text-xs
-
-		border-0
-
-		focus:outline-none
-		focus:border-0
-	"
     />
 </div>
