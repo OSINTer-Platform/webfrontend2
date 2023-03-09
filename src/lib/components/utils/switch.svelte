2bbc21035d4b0979f153cc1fa008a04daa32fc11
bertmad3400 <skrivtilbertram@gmail.com>
2023-03-09 14:42:41 +0100

    Add util component for switch style checkbox

---
+++ src/lib/components/utils/switch.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let checked: boolean = false;
+    export let size: 'xs' | 'sm' | 'base' | 'md' | 'lg' | 'xl' | '2xl' = 'base';
+    export let name = '';
+</script>
+
+<label class="group relative cursor-pointer text-{size}">
+    <input type="checkbox" class="peer hidden" bind:checked on:change {name} />
+    <span
+        class="
+		flex items-center
+		p-[0.1875em] h-[1.5em] w-[2.25em]
+		rounded-full bg-tertiary-400
+		duration-300 ease-in-out
+
+		after:h-[1.125em] after:w-[1.125em]
+		after:rounded-full after:bg-white after:shadow-md
+		after:duration-300
+
+		group-hover:after:translate-x-[0.1875em]
+		peer-checked:after:translate-x-[0.75em]
+		peer-checked:bg-primary-500
+	"
+    />
+</label>
