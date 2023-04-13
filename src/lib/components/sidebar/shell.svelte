b9dad94343d039955396a609d9652fffadcc5399
bertmad3400 <skrivtilbertram@gmail.com>
2023-04-13 13:06:14 +0200

    Animate sidebar

--- src/lib/components/sidebar/shell.svelte
+++ src/lib/components/sidebar/shell.svelte
@@ -23,23 +23,30 @@
     on:click={() => ($sideOpen = !$sideOpen)}
     on:keyup={() => ($sideOpen = !$sideOpen)}
     class="
-	{$sideOpen ? 'flex' : 'hidden'}
+	{$sideOpen ? 'opacity-100 visible' : 'opacity-0 invisible'}
 	lg:hidden
 
+	flex
 	absolute
 	z-40
 
-	w-screen
 	bottom-0
 	top-0
 
+	w-screen
+
 	bg-black/25
+
+	transition-all
+	duration-200
 "
 >
     <div
         on:click|stopPropagation
         on:keyup|stopPropagation
         class="
+		{$sideOpen ? 'max-w-full' : 'max-w-0'}
+
 		grid
 		grid-cols-[auto_1fr]
 
@@ -48,6 +55,9 @@
 		bg-surface-50
 
 		overflow-auto
+
+		transition-[max-width]
+		duration-200
 	"
     >
         <slot />
