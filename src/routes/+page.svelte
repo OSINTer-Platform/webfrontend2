b6247a8f1a92503fd1a39b5e7da4c5f2fcefcb42
bertmad3400 <skrivtilbertram@gmail.com>
2023-05-11 16:13:01 +0200

    Add notice about project being in development

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+    import Fa from 'svelte-fa/src/fa.svelte';
+    import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
+
     import InternalLinks from '$com/frontpage/internalLinks.svelte';
     import { config } from '$shared/config';
 </script>
@@ -31,15 +34,51 @@
 		w-full
 	"
     >
-        <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <InternalLinks />
+        <section>
+            <div
+                class="
+				p-4
+
+				flex
+				items-center
+				gap-8
+
+				md:text-lg
+				font-medium
+				bg-primary-400/20
+				border-4
+				border-primary-800/20
+
+				rounded-md
+			"
+            >
+                <Fa
+                    icon={faExclamationCircle}
+                    class="text-3xl text-primary-800"
+                />
+                Do keep in mind that OSINTer 5.0 is still under development and testing,
+                and stability may vary.
+            </div>
+
+            <hr class="" />
+
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <InternalLinks />
+            </div>
+
+            <hr class="" />
         </section>
     </main>
 </div>
 
 <style lang="scss">
+    hr {
+        @apply text-surface-400/10 border-2;
+    }
+
     section {
         @apply xl:max-w-6xl mx-auto
-		p-4 sm:p-8 lg:p-12 xl:py-20;
+		p-4 sm:p-8 lg:p-12 xl:py-20
+		space-y-12;
     }
 </style>
