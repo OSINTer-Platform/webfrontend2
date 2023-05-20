33f139236a0313eedf9b5f4bcfc5d2ae7d27ae35
bertmad3400 <skrivtilbertram@gmail.com>
2023-05-20 20:41:42 +0200

    Use proper type for user item

--- src/routes/(sidebar)/(app)/feed/(list)/[item_id=uuid]/+layout.ts
+++ src/routes/(sidebar)/(app)/feed/(list)/[item_id=uuid]/+layout.ts
@@ -1,10 +1,10 @@
 import { PUBLIC_API_BASE } from '$env/static/public';
 import { handleResponse } from '$lib/common/query';
-import type { ItemBase } from '$shared/types/userItems';
+import type { Collection, Feed } from '$shared/types/userItems';
 import type { LayoutLoad } from './$types';
 
 export const load = (async ({ params, fetch }) => {
-    const fetchItem = async (): Promise<ItemBase> => {
+    const fetchItem = async (): Promise<Feed | Collection> => {
         const r = await fetch(
             `${PUBLIC_API_BASE}/user-items/${params.item_id}/content`
         );
