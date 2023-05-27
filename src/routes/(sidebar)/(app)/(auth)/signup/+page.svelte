962083ae77bf428f4129a379d87f169e6ef2fcbc
bertmad3400 <skrivtilbertram@gmail.com>
2023-05-27 19:58:18 +0200

    Add functionality for sign-up

--- src/routes/(sidebar)/(app)/(auth)/signup/+page.svelte
+++ src/routes/(sidebar)/(app)/(auth)/signup/+page.svelte
@@ -2,6 +2,10 @@
     import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
     import Fa from 'svelte-fa/src/fa.svelte';
 
+    import type { ActionData } from './$types';
+
+    export let form: ActionData;
+
     let username: string = '';
     let password: string = '';
     let repeatPassword: string = '';
@@ -19,6 +23,12 @@
             formError = null;
         }
     }
+
+    let success = form?.success ?? true;
+    let title = success ? 'Hi there!' : 'Failure!';
+    let detail =
+        form?.detail ??
+        'Sign up down below to start with your journey into the wonderful world of CTI';
 </script>
 
 <main
@@ -39,11 +49,8 @@
 "
 >
     <header class="dark:text-white self-stretch">
-        <h1 class="text-5xl font-semibold">Hi there!</h1>
-        <p class="font-light">
-            Sign up below to start your own journey into the wonderful world of
-            CTI
-        </p>
+        <h1 class="text-5xl font-semibold">{title}</h1>
+        <p class="font-light">{detail}</p>
     </header>
 
     <form method="post" class="w-full">
