5f0a68a627431fd58554d68524014507328394a2
bertmad3400 <skrivtilbertram@gmail.com>
2023-05-29 20:13:44 +0200

    Add search modal

--- src/lib/components/modals/all.svelte
+++ src/lib/components/modals/all.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
     import { modalState } from '$state/state';
     import Article from './article.svelte';
+    import Search from './search.svelte';
 </script>
 
 {#if $modalState.modalType == 'article'}
     <Article
         article={$modalState.modalContent.article}
         articleCategories={$modalState.modalContent.categories}
+    />
+{:else if $modalState.modalType == 'search'}
+    <Search
+        searchQuery={$modalState.modalContent.query}
+        callback={$modalState.modalContent.searchAction}
+        searchText={$modalState.modalContent.searchText}
     />
 {/if}
