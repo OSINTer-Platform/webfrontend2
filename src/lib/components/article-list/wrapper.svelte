4115ed48e2a8848f6e047746903586f09f32ed04
bertmad3400 <skrivtilbertram@gmail.com>
2023-05-30 13:37:17 +0200

    Add notice when feeds or collections are empty

--- src/lib/components/article-list/wrapper.svelte
+++ src/lib/components/article-list/wrapper.svelte
@@ -17,5 +17,19 @@
 	sm:p-12
 "
 >
-    <List {articles} layout={$articleListRender} />
+    {#if articles.length > 0}
+        <List {articles} layout={$articleListRender} />
+    {:else}
+        <div
+            class="h-full mx-auto px-8 max-w-2xl flex flex-col justify-center text-center dark:text-white"
+        >
+            <h2 class="md:text-4xl sm:text-3xl text-2xl font-bold md:mb-2">
+                Well, it looks rather empty here...
+            </h2>
+            <p class="md:text-xl sm:text-lg font-light md:tracking-tighter">
+                Try using another feed or collection, or contact your system
+                administrator if you believe this is an error.
+            </p>
+        </div>
+    {/if}
 </div>
