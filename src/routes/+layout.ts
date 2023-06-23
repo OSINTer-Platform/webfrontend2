bcc04360b580a8086804f0b169ff5df5a413b4e0
bertmad3400 <skrivtilbertram@gmail.com>
2023-06-23 10:49:58 +0000

    Move user data loading function to root layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,17 @@
+import { PUBLIC_API_BASE } from "$env/static/public";
+import type { User } from "$shared/types/userItems";
 import type { LayoutLoad } from "./$types";
 
 export const load = (async ({ route }) => {
   const pagesWithSidebar = /^\/\(sidebar\)\/(\(app\)\/)?(feed|article).*/;
+
+  const getUserObject = async (): Promise<User | null> => {
+    const r = await fetch(`${PUBLIC_API_BASE}/auth/status`);
+    return r.ok ? r.json() : null;
+  };
+
   return {
+    user: getUserObject(),
     burgerMenu: route.id !== "/",
     customSidebar: route.id ? pagesWithSidebar.test(route.id) : false,
   };
