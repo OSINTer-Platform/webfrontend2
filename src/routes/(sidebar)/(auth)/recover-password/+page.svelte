9a926591965552833d21a684e62dfd0c472b516b
bertmad3400 <skrivtilbertram@gmail.com>
2023-06-27 11:43:50 +0000

    Add standin page for recover password

---
+++ src/routes/(sidebar)/(auth)/recover-password/+page.svelte
@@ -0,0 +1,21 @@
+<header class="dark:text-white self-stretch">
+  <h1 class="text-5xl font-semibold mb-6">Please contact your sysadmin</h1>
+
+  <hr class="text-surface-400/25 border my-10 w-full" />
+
+  <p class="font-light text-justify">
+    As this instance of OSINTer at this time doesn't support automatic password
+    reset, you will have to contact the system administrator responsible for
+    this instance to reset your password
+  </p>
+</header>
+
+<hr class="text-surface-400/25 border my-10 w-full" />
+
+<p class="font-light block w-fit mt-4 dark:text-white">
+  Want to try again? <a
+    href="/login"
+    class="link-underline text-primary-600 dark:text-primary-400 w-fit inline-block"
+    >Login in here</a
+  >
+</p>
