4c245621faad95b59c2ea380fd74ce7898064dee
bertmad3400 <skrivtilbertram@gmail.com>
2023-06-29 13:19:52 +0000

    Add filtering based on id to filterArticle function

--- src/lib/common/filter.ts
+++ src/lib/common/filter.ts
@@ -1,8 +1,13 @@
 import type { ArticleBase } from "$shared/types/api";
 
+/**
+ * includeIds control whether to INCLUDE or EXCLUDE articles with id contained in ids param
+ */
 export function filterArticles<T extends ArticleBase>(
   articles: T[],
-  search: string
+  search: string,
+  ids: string[] = [],
+  includeIds: boolean = false
 ): T[] {
   search = search.toLowerCase();
 
@@ -11,6 +16,12 @@ export function filterArticles<T extends ArticleBase>(
       Object.values(article).some((field) =>
         field?.toLowerCase?.().includes?.(search)
       )
+    );
+  }
+
+  if (ids.length > 0) {
+    articles = articles.filter(
+      (article) => ids.includes(article.id) == includeIds
     );
   }
 
