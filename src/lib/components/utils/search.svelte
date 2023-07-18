378aec55b401dde0cafdcc781114335209062e4f
bertmad3400 <skrivtilbertram@gmail.com>
2023-07-18 10:30:36 +0000

    Allow for requesting search using local search bars

--- src/lib/components/utils/search.svelte
+++ src/lib/components/utils/search.svelte
@@ -8,30 +8,33 @@
   export let inputClass: string = "";
 </script>
 
-<div
+<form
   class="
-	flex items-center
+    flex items-center
 
-	border border-tertiary-600
-	bg-surface-600/10 text-black
-	rounded-md
+    border border-tertiary-600
+    bg-surface-600/10 text-black
+    rounded-md
 
-	{containerClass}
-"
+    {containerClass}
+  "
+  action="/feed/search"
+  method="get"
 >
-  <label for="search-field" class="text-tertiary-800 m-2">
+  <label for="search_term" class="text-tertiary-800 m-2">
     <Fa icon={faMagnifyingGlass} />
   </label>
 
   <input
-    id="search-field"
+    id="search_term"
+    name="search_term"
     bind:value
     {placeholder}
     class="
-		grow h-full
-		bg-transparent placeholder-tertiary-800 text-xs dark:text-white
-		border-0 focus:outline-none focus:border-0
-		{inputClass}
-	"
+    grow h-full
+    bg-transparent placeholder-tertiary-800 text-xs dark:text-white
+    border-0 focus:outline-none focus:border-0
+    {inputClass}
+  "
   />
-</div>
+</form>
