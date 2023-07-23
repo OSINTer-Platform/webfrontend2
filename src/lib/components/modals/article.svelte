acea5b426a0fabc3b948d26ae0733d683eec80c1
bertmad3400 <skrivtilbertram@gmail.com>
2023-07-23 13:28:28 +0000

    Allow for switching between articles in modal using arrows

--- src/lib/components/modals/article.svelte
+++ src/lib/components/modals/article.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
-  import type { Article, ArticleCategories } from "$shared/types/api";
+  import type {
+    Article,
+    ArticleBase,
+    ArticleCategories,
+  } from "$shared/types/api";
   import Modal from "./modal.svelte";
   import ArticleRender from "$com/articleRender/main.svelte";
+  import { page } from "$app/stores";
+  import { spawnArticleModal } from "$lib/common/state";
 
   export let article: Article;
   export let articleCategories: ArticleCategories;
+
+  async function handleKeypress(keyName: string) {
+    if (keyName !== "ArrowRight" && keyName !== "ArrowLeft") return;
+
+    const articles: ArticleBase[] = $page.data.articles;
+    if (!articles) return;
+
+    const currentArticleNr = articles.map((a) => a.id).indexOf(article.id);
+    if (currentArticleNr < 0) return;
+
+    let newArticleId: string = article.id;
+    if (keyName === "ArrowLeft")
+      newArticleId = articles[currentArticleNr - 1].id;
+    if (keyName === "ArrowRight")
+      newArticleId = articles[currentArticleNr + 1].id;
+
+    spawnArticleModal(newArticleId);
+  }
 </script>
+
+<svelte:window on:keydown={(e) => handleKeypress(e.key)} />
 
 <Modal class="w-[80vw] h-[90vh] bg-surface-200 dark:bg-surface-700">
   <article
