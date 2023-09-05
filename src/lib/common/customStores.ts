b6091971d3a7e79671b0f53291a15c7ce9eb6827
bertmad3400 <skrivtilbertram@gmail.com>
2023-09-05 11:24:34 +0000

    Create custom reset-able store
    This also includes the functionality for transition reset-able stores containing number from current to original number

--- src/lib/common/customStores.ts
+++ src/lib/common/customStores.ts
@@ -1,4 +1,4 @@
-import { writable, type Writable } from "svelte/store";
+import { get, writable, type Writable } from "svelte/store";
 
 export interface Updatable<T> extends Writable<T> {
   autoUpdate: () => void;
@@ -20,5 +20,57 @@ export async function updatable<T>(
     set,
     update,
     autoUpdate,
+  };
+}
+
+export interface WritableWithDefault<ContentType>
+  extends Writable<ContentType> {
+  reset: (duration?: number, steps?: number) => void;
+  contentDefault: ContentType;
+}
+export function writableWithDefault<ContentType>(
+  contentDefault: ContentType
+): WritableWithDefault<ContentType> {
+  const internalStore: Writable<ContentType> = writable(contentDefault);
+
+  async function taperNumber(duration: number, numberDefault: number) {
+    const prevVal = get(internalStore);
+
+    if (typeof prevVal !== "number") {
+      throw "Type-mismatch between current content and default content";
+    }
+
+    const start = performance.now();
+    const stop = start + duration;
+
+    const step = (timeStamp: number) => {
+      const elapsed = timeStamp - start;
+      const progress = Math.min(elapsed / duration, 1);
+      const value = prevVal + (numberDefault - prevVal) * progress;
+
+      (internalStore as Writable<number>).set(value);
+
+      if (timeStamp < stop) {
+        requestAnimationFrame(step);
+      }
+    };
+
+    requestAnimationFrame(step);
+  }
+
+  async function reset(duration: number = 0) {
+    if (duration > 0 && typeof contentDefault === "number") {
+      await taperNumber(duration, contentDefault);
+    } else {
+      internalStore.set(contentDefault);
+    }
+  }
+
+  return {
+    subscribe: internalStore.subscribe,
+    set: internalStore.set,
+    update: internalStore.update,
+    reset,
+    contentDefault,
   };
 }
