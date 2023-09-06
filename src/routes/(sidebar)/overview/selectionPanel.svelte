f4c3dc6f937bb5237c0db6727f67d27346d71528
bertmad3400 <skrivtilbertram@gmail.com>
2023-09-06 20:20:31 +0000

    Implement different pointer mode and controls for it

--- src/routes/(sidebar)/overview/selectionPanel.svelte
+++ src/routes/(sidebar)/overview/selectionPanel.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
   import * as d3 from "d3";
   import { controlParams, d3Selection, d3Zoom } from "./state";
+  import type { PointerModes } from "./state";
 
-  const { dotSize, toolTipSize, deepSearch, search, enableSearch } =
-    controlParams;
+  import Fa from "svelte-fa/src/fa.svelte";
+  import { faObjectGroup } from "@fortawesome/free-regular-svg-icons";
+  import {
+    faMagnifyingGlass,
+    faUpDownLeftRight,
+    type IconDefinition,
+  } from "@fortawesome/free-solid-svg-icons";
+
+  const {
+    dotSize,
+    toolTipSize,
+    deepSearch,
+    search,
+    enableSearch,
+    pointerMode,
+  } = controlParams;
 
   function resetMap() {
     dotSize.reset(300);
@@ -19,6 +34,23 @@
         .call($d3Zoom.transform, d3.zoomIdentity);
     }
   }
+
+  const pointerModes: Array<{
+    icon: IconDefinition;
+    name: PointerModes;
+    description: string;
+  }> = [
+    {
+      icon: faUpDownLeftRight,
+      name: "pan",
+      description: "Pan and zoom across the map",
+    },
+    {
+      icon: faObjectGroup,
+      name: "select",
+      description: "Select objects from the map",
+    },
+  ];
 </script>
 
 <aside
@@ -33,10 +65,42 @@
   <button
     on:click={resetMap}
     class="
-    btn py-2
+    btn mb-4 h-14
     font-bold text-xl
     border-surface-400 border
     dark:bg-surface-600/80
   ">Reset</button
   >
+
+  <div
+    class="
+    flex h-14
+    border-surface-400 border
+    dark:bg-surface-600/80
+    "
+  >
+    {#each pointerModes as { icon, name, description }}
+      <label
+        title={description}
+        class="
+        btn active:scale-100
+        grow
+        text-xl
+
+        {$pointerMode === name
+          ? 'bg-primary-600/60 hover:bg-primary-500/60'
+          : ''}
+      "
+      >
+        <input
+          type="radio"
+          bind:group={$pointerMode}
+          name="pointer-mode-selection"
+          value={name}
+          class="hidden"
+        />
+        <Fa {icon} />
+      </label>
+    {/each}
+  </div>
 </aside>
--- src/routes/(sidebar)/overview/state.ts
+++ src/routes/(sidebar)/overview/state.ts
@@ -2,8 +2,11 @@ import {
   writableWithDefault,
   type WritableWithDefault,
 } from "$lib/common/customStores";
+import type { MLArticle } from "$shared/types/api";
 import * as d3 from "d3";
 import { writable, type Writable } from "svelte/store";
+
+export type PointerModes = "pan" | "select";
 
 export const controlParams: {
   dotSize: WritableWithDefault<number>;
@@ -11,15 +14,19 @@ export const controlParams: {
   enableSearch: WritableWithDefault<boolean>;
   deepSearch: WritableWithDefault<boolean>;
   search: WritableWithDefault<string>;
+  pointerMode: WritableWithDefault<PointerModes>;
 } = {
   dotSize: writableWithDefault(1),
   toolTipSize: writableWithDefault(14),
   enableSearch: writableWithDefault(true),
   deepSearch: writableWithDefault(false),
   search: writableWithDefault(""),
+  pointerMode: writableWithDefault("pan"),
 };
 
 export const toolTips: Writable<string[]> = writable([]);
+export const selectedArticles: Writable<MLArticle[]> = writable([]);
+
 export const mouseX: Writable<{ actual: number; translated: number }> =
   writable({
     actual: 0,
@@ -34,6 +41,13 @@ export const mapTransform: Writable<d3.ZoomTransform> = writable(
   d3.zoomIdentity
 );
 
+export const selectionBoundaries: {
+  [key in "start" | "end"]: Writable<null | { x: number; y: number }>;
+} = {
+  start: writable(null),
+  end: writable(null),
+};
+
 export const d3Selection: Writable<d3.Selection<
   HTMLCanvasElement,
   unknown,
@@ -43,5 +57,11 @@ export const d3Selection: Writable<d3.Selection<
 
 export const d3Zoom: Writable<d3.ZoomBehavior<
   HTMLCanvasElement,
+  unknown
+> | null> = writable(null);
+
+export const d3Drag: Writable<d3.DragBehavior<
+  HTMLCanvasElement,
+  unknown,
   unknown
 > | null> = writable(null);
