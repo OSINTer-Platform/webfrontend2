2cc3cd8f31ad1d0ab2191fad8775d5e3da8bdf36
bertmad3400 <skrivtilbertram@gmail.com>
2023-09-11 17:41:34 +0000

    Reroute user to full article page if data loadign for modal fails

--- src/lib/common/state.ts
+++ src/lib/common/state.ts
@@ -6,15 +6,27 @@ import { get } from "svelte/store";
 
 import type { Article, ArticleCategories } from "$shared/types/api";
 import { PUBLIC_API_BASE } from "$env/static/public";
+import { goto } from "$app/navigation";
 
 export async function spawnArticleModal(id: string) {
-  const fetchAndConvert = (url: string) => fetch(url).then((r) => r.json());
+  const fetchAndConvert = (url: string) =>
+    fetch(url).then((r) => {
+      if (!r.ok) return null;
+      return r.json();
+    });
 
-  const [article, articleCategories]: [Article, ArticleCategories] =
-    await Promise.all([
-      fetchAndConvert(`${PUBLIC_API_BASE}/articles/${id}/content`),
-      fetchAndConvert(`${PUBLIC_API_BASE}/articles/categories`),
-    ]);
+  const [article, articleCategories]: [
+    Article | null,
+    ArticleCategories | null
+  ] = await Promise.all([
+    fetchAndConvert(`${PUBLIC_API_BASE}/articles/${id}/content`),
+    fetchAndConvert(`${PUBLIC_API_BASE}/articles/categories`),
+  ]);
+
+  if (!article || !articleCategories) {
+    goto(`/article/${id}`);
+    return false;
+  }
 
   fullArticles.update((list) => {
     list[id] = article;
@@ -32,4 +44,6 @@ export async function spawnArticleModal(id: string) {
     pageData.userCollections.autoUpdate(),
     pageData.alreadyRead.autoUpdate(),
   ]);
+
+  return true;
 }
