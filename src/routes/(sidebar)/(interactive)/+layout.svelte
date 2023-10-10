f488315d600f1b18c231edae73ccdea616fcab1a
bertmad3400 <skrivtilbertram@gmail.com>
2023-10-10 09:11:31 +0000

    Make group layout for requirering Javascript

---
+++ src/routes/(sidebar)/(interactive)/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import { onMount } from "svelte";
+
+  let javascript = false;
+
+  onMount(() => {
+    if (browser) {
+      javascript = true;
+    }
+  });
+</script>
+
+{#if javascript}
+  <slot />
+{:else}
+  <main class="w-full h-full bg-surface-100 dark:bg-surface-800">
+    <div
+      class="h-full mx-auto px-8 xl:max-w-5xl max-w-2xl flex flex-col justify-center text-center dark:text-white"
+    >
+      <h2
+        class="xl:text-5xl md:text-4xl sm:text-3xl text-2xl font-bold md:mb-2"
+      >
+        Javascript has to be enabled
+      </h2>
+      <p
+        class="xl:text-2xl md:text-xl sm:text-lg font-light md:tracking-tighter"
+      >
+        It doesn't seem like Javascript is enabled - or something is interfering
+        with it. Please turn Javascript back on to use this page.
+      </p>
+    </div>
+  </main>
+{/if}
--- src/routes/(sidebar)/(interactive)/overview/+page.svelte
+++ src/routes/(sidebar)/(interactive)/overview/+page.svelte
@@ -160,21 +160,5 @@
         >
       </div>
     {/await}
-  {:else}
-    <div
-      class="h-full mx-auto px-8 xl:max-w-5xl max-w-2xl flex flex-col justify-center text-center dark:text-white"
-    >
-      <h2
-        class="xl:text-5xl md:text-4xl sm:text-3xl text-2xl font-bold md:mb-2"
-      >
-        Javascript has to be enabled
-      </h2>
-      <p
-        class="xl:text-2xl md:text-xl sm:text-lg font-light md:tracking-tighter"
-      >
-        It doesn't seem like Javascript is enabled - or something is interfering
-        with it. Please turn Javascript back on to use this page.
-      </p>
-    </div>
   {/if}
 </div>
