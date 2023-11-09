2b864a1ef70a9f52e807899bb9be343211ee786c
bertmad3400 <skrivtilbertram@gmail.com>
2023-11-09 09:39:35 +0000

    Allow icons inside switch input

--- src/lib/components/utils/inputs/switch.svelte
+++ src/lib/components/utils/inputs/switch.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+  import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
+  import Fa from "svelte-fa";
+
   export let checked: boolean = false;
   export let size: "xs" | "sm" | "base" | "md" | "lg" | "xl" | "2xl" = "base";
   export let name: string = "";
   export let containerClass: string = "";
   export let title: string = "";
+
+  export let icons: null | { off: IconDefinition; on: IconDefinition } = null;
 </script>
 
 <label
@@ -22,17 +27,31 @@
     class="
 		flex items-center
 		p-[0.1875em] h-[1.5em] w-[2.25em]
-		rounded-full bg-tertiary-400 dark:bg-surface-600 dark:border dark:border-white/25
+		rounded-full
+
+    bg-surface-300 dark:bg-surface-600
+    dark:border dark:border-white/25
 		duration-300 ease-in-out
 
-		after:h-[1.125em] after:w-[1.125em]
-		after:rounded-full after:bg-white after:shadow-md
-		after:duration-300
-
-		group-hover:after:translate-x-[0.1875em]
-		peer-checked:after:translate-x-[0.75em]
 		peer-checked:bg-primary-500
+    group-hover:[&>div]:translate-x-[0.1875em]
+    peer-checked:[&>div]:translate-x-[0.75em]
 	"
-  />
+  >
+    <div
+      class="
+      flex justify-center items-center
+      absolute h-[1.125em] w-[1.125em]
+      rounded-full shadow-md
+		  duration-300 z-10
+      bg-white
+  "
+    >
+      {#if icons}
+        <Fa icon={checked ? icons.on : icons.off} class="w-3/4 aspect-square" />
+      {/if}
+    </div>
+  </span>
+
   <slot />
 </label>
