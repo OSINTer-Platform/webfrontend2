e651e57c8d73ed3cbbd3d2055b74560a5fb5c30a
bertmad3400 <skrivtilbertram@gmail.com>
2023-11-09 12:02:24 +0000

    Only show expanded modlist on large screens

--- src/lib/components/article-list/header/modList.svelte
+++ src/lib/components/article-list/header/modList.svelte
@@ -12,12 +12,12 @@
 </script>
 
 <ListMenu listOptions={[modOptions]}>
-  <button class="{btnClass} md:hidden block">
+  <button class="{btnClass} 2xl:hidden block">
     <Fa icon={faBars} />
   </button>
 </ListMenu>
 
-<div class="md:flex hidden gap-4">
+<div class="2xl:flex hidden gap-4">
   {#each modOptions as { title, icon, route, action }}
     {#if route}
       <a href={route} {title} class={btnClass}><Fa {icon} /></a>
