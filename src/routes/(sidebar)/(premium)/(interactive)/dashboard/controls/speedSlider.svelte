c0bf6aa37d6e87cd0c2d0d89fbcbd0a55f5bdc50
bertmad3400 <skrivtilbertram@gmail.com>
2023-12-12 10:34:23 +0000

    Add label to speed slider

--- src/routes/(sidebar)/(premium)/(interactive)/dashboard/controls/speedSlider.svelte
+++ src/routes/(sidebar)/(premium)/(interactive)/dashboard/controls/speedSlider.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
+  import * as d3 from "d3";
+
   export let value: number;
+  let clientWidth: number = 0;
+  let min = 0.2;
+  let max = 6;
+
+  $: x = d3
+    .scaleLinear()
+    .domain([min, max])
+    .range([16, clientWidth - 32]);
+
+  $: tooltipX = x(value);
 </script>
 
-<div
-  class="w-1/3 md:w-1/5 flex rounded-full bg-black"
-  title="Control scroll speed"
->
+<div class="w-1/3 md:w-1/5 flex rounded-full bg-black" bind:clientWidth>
   <input
     on:change
     bind:value
     type="range"
-    min="1"
-    max="5"
+    {min}
+    {max}
     step="0.2"
     class="
       basis-0 w-4 shrink grow h-2 my-4 mx-4
@@ -26,4 +35,19 @@
       peer
     "
   />
+
+  <div
+    style="left: {tooltipX + 8}px;"
+    class="
+    opacity-0 peer-hover:opacity-100
+    transition-opacity duration-300
+    absolute -top-16 -translate-x-1/2
+    p-2 rounded-sm
+    bg-black w-max text-white
+    flex flex-col text-center
+  "
+  >
+    <span class="font-light text-xs">Speed</span>
+    <span>{value}x</span>
+  </div>
 </div>
