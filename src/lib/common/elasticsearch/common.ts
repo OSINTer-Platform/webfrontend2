fd58d4e726a2dfd4aa618a54997ffae17e1a1b47
bertmad3400 <skrivtilbertram@gmail.com>
2024-01-21 19:31:12 +0000

    Include highlights when searching articles in ES

--- src/lib/common/elasticsearch/common.ts
+++ src/lib/common/elasticsearch/common.ts
@@ -16,4 +16,5 @@ export const extractDocHits = (response: any) =>
   response.hits.hits.map((hit: any) => ({
     id: hit._id,
     ...(hit._source as object),
+    highlights: hit.highlight,
   }));
