d6d29a5d0b1a656157cbf3632ee9a250b0cf8368
bertmad3400 <skrivtilbertram@gmail.com>
2024-01-25 16:12:33 +0000

    Bugfix: Put checkbox in label to avoid spotty updating

--- src/lib/components/collections/list.svelte
+++ src/lib/components/collections/list.svelte
@@ -11,7 +11,7 @@
 
   const handleCheckbox = async (
     e: (MouseEvent | KeyboardEvent) & {
-      currentTarget: EventTarget & HTMLLIElement;
+      currentTarget: EventTarget & HTMLLabelElement;
     }
   ) => {
     const collectionId = e.currentTarget.id;
@@ -40,63 +40,57 @@
 </script>
 
 <ul class={containerClass}>
-  {#each Object.values($userCollections) as collection}
-    {#if collectionSearch.length == 0 || collection.name
+  {#each Object.values($userCollections) as { _id, ids, name }}
+    {#if collectionSearch.length == 0 || name
         .toLowerCase()
         .includes(collectionSearch.toLowerCase())}
       <li
-        id={collection._id}
         class="
-        p-2
         w-full min-w-max
-        flex items-center
-
-        text-left
-        rounded-md
-
-        space-x-2
-
-        cursor-pointer
-
-        hover:bg-primary-500/10
-        [&:hover>input]:bg-primary-500/20
-        [&:hover>input:checked]:bg-primary-600/75
-        [&:hover>label>p>span.opacity-0]:opacity-75
       "
-        on:click={handleCheckbox}
-        on:keydown={handleCheckbox}
       >
-        <input
-          type="checkbox"
-          id="collection-{collection._id}"
-          checked={collection.ids.includes(articleId)}
-          class="w-4 checkbox"
-        />
-
         <label
-          on:click|stopPropagation
-          on:keydown|stopPropagation
-          for="collection-{collection._id}"
+          id={_id}
+          on:click|stopPropagation|preventDefault={handleCheckbox}
+          on:keydown|stopPropagation|preventDefault={handleCheckbox}
+          for="collection-{_id}"
           class="
-            cursor-pointer
-            font-light dark:font-normal
-            dark:text-white
+            p-2
+            flex items-center
+
+            cursor-pointer rounded-md
+
+            text-left font-light dark:font-normal dark:text-white
             {showStats ? 'text-sm md:text-base' : 'text-xs md:text-sm'}
+
+            hover:bg-primary-500/10
+            [&:hover>input]:bg-primary-500/20
+            [&:hover>input:checked]:bg-primary-600/75
+            [&:hover>p>span.opacity-0]:opacity-75
           "
-          >{collection.name}
+        >
+          <input
+            type="checkbox"
+            id="collection-{_id}"
+            checked={ids.includes(articleId)}
+            class="w-4 checkbox mr-2"
+          />
+          {name}
           {#if showStats}
-            <p class="inline text-black/50 dark:text-white/50 text-xs">
+            <p class="pl-1 inline text-black/50 dark:text-white/50 text-xs">
               <span>
-                | {collection.ids.length} articles
+                | {ids.length} articles
               </span>
               <span class="opacity-0 transition-opacity hidden md:inline">
-                | {collection._id}
+                | {_id}
               </span>
             </p>
           {/if}
         </label>
       </li>
-      <hr class="border-surface-500/25 last:hidden my-1" />
+      <hr
+        class="border-surface-400/25 last:hidden {showStats ? 'my-2' : 'my-1'}"
+      />
     {/if}
   {/each}
 </ul>
