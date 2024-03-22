72c0bee97a63527d0ebd7257970e4431c0d46d6f
bertmad3400 <skrivtilbertram@gmail.com>
2024-03-22 14:08:10 +0000

    Add types for stripe details from osinter api

---
+++ src/lib/shared/types/stripe.ts
@@ -0,0 +1,7 @@
+export type Price = {
+  id: string;
+  unit_amount: number;
+  currency: string;
+  lookup_key: string;
+};
+export type Products = { id: string; name: string; description: number };
