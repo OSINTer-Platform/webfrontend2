7ae1024cf64c25e3c7dafef8540e4ec4c5bed611
bertmad3400 <skrivtilbertram@gmail.com>
2024-03-24 11:56:17 +0000

    Customize premium wall based on whether user is logged in

--- src/routes/(sidebar)/(premium)/+layout.ts
+++ src/routes/(sidebar)/(premium)/+layout.ts
@@ -31,12 +31,17 @@ export const load: LayoutLoad = async ({ parent, url }) => {
   if (!userContent || !(userContent.premium > 0))
     throw error(403, {
       message: "",
-      title: "This page is reserved for beta-testers and B2B partners.",
+      title: `This page is reserved${
+        userContent
+          ? " for beta-testers and B2B partners."
+          : ", and you are not logged in."
+      }`,
       description: [
         "We are at OSINTer currently beta-testing new features",
         "Do you want early access?",
-        "Contact us below",
+        userContent ? "Contact us below" : "Log in below",
       ],
       logo: false,
+      actions: userContent ? undefined : [{ title: "Login", href: "/login" }],
     });
 };
