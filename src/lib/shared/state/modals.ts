c4356c38a4db193e5af2800d0fe2bc5efa7bc102
bertmad3400 <skrivtilbertram@gmail.com>
2024-03-24 11:11:51 +0000

    Allow removal of modal by id from list store

--- src/lib/shared/state/modals.ts
+++ src/lib/shared/state/modals.ts
@@ -7,18 +7,32 @@ type OptionalId<T> = Omit<T, "id"> & { id?: string };
 interface AddIdListStore<T> extends Omit<ListStore<T>, "append" | "prepend"> {
   append: (el: OptionalId<T>) => void;
   prepend: (el: OptionalId<T>) => void;
+  remove: (id?: string) => T | undefined;
 }
 
 function addIdListStore<T extends { id: string }>(
   initialVal: T[]
 ): AddIdListStore<T> {
   const store = listStore<T>(initialVal);
-  const { set, update, subscribe, remove } = store;
+  const { set, update, subscribe } = store;
 
   const addId = (el: OptionalId<T>): T => ({ id: uuid(), ...el } as T);
 
   const append = (el: OptionalId<T>) => store.append(addId(el));
   const prepend = (el: OptionalId<T>) => store.prepend(addId(el));
+
+  function remove(id?: string) {
+    if (!id) return store.remove();
+
+    let el: T | undefined;
+    update((list) => {
+      const i = list.findIndex((el) => el.id == id);
+      el = list[i];
+      list.splice(i, 1);
+      return list;
+    });
+    return el;
+  }
 
   return {
     set,
