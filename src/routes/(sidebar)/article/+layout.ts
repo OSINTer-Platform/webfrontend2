15264486380f4091374cd7b283ad065498252ad3
bertmad3400 <skrivtilbertram@gmail.com>
2024-04-28 07:11:01 +0000

    Require user to be logged in for article render page

--- src/routes/(sidebar)/article/+layout.ts
+++ src/routes/(sidebar)/article/+layout.ts
@@ -1,6 +1,26 @@
 import type { LayoutLoad } from "./$types";
+import { error } from "@sveltejs/kit";
+import { get } from "svelte/store";
 
-export const load: LayoutLoad = async () => {
+export const load: LayoutLoad = async ({ parent }) => {
+  const parentData = await parent();
+  const user = get(parentData.user);
+
+  if (!user)
+    throw error(401, {
+      message: "",
+      title: "You have to be logged in to access this article",
+      description: [
+        "Full-page rendering are only available to logged-in users",
+        "Do you want to try it out?",
+        "Log in or sign-up below",
+      ],
+      logo: false,
+      actions: [
+        { title: "Login", href: "/login" },
+        { title: "Sign-up", href: "/signup" },
+      ],
+    });
   return {
     customSidebar: true,
     meta: {
