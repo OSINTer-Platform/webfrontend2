62d355e68f8ce152eedd7d9b4430e865605f5b29
bertmad3400 <skrivtilbertram@gmail.com>
2024-05-06 09:36:19 +0000

    Make link-list sidebar component substantially more modular

--- src/lib/components/sidebar/linkList.svelte
+++ src/lib/components/sidebar/linkList.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { writable } from "svelte/store";
+
   import type { SidebarOption } from "$shared/types/internal";
 
   import AppSwitcher from "./compontents/appSwitcher.svelte";
@@ -8,7 +10,25 @@
 
   import SidebarShell from "./compontents//shell.svelte";
 
-  export let options: Array<SidebarOption> = [];
+  import Loader from "$com/loader.svelte";
+  import Search from "$com/utils/inputs/search.svelte";
+
+  type Doc = $$Generic;
+
+  export let docs: Promise<Doc[]> | Doc[];
+  export let generateOptionLink: (doc: Doc) => { href: string; label: string };
+  export let searchFilter: (doc: Doc, search: string) => boolean;
+  export let type;
+  export let search = writable("");
+
+  function generateOption(docs: Doc[], search: string): SidebarOption {
+    return {
+      id: "link-list",
+      list: Object.values(docs)
+        .filter((doc) => searchFilter(doc, search))
+        .map((doc) => generateOptionLink(doc)),
+    };
+  }
 </script>
 
 <SidebarShell>
@@ -17,16 +37,17 @@
   </svelte:fragment>
 
   <svelte:fragment slot="content">
-    {#if options.length > 0}
-      <LinkNavShell>
-        <slot name="top" />
-        {#each options as option}
-          {#if option.list.length > 0}
-            <LinkNavOptions options={option} />
-          {/if}
-        {/each}
-        <slot name="bottom" />
-      </LinkNavShell>
-    {/if}
+    <LinkNavShell>
+      {#await docs}
+        <Loader text="Loading previous {type}s" />
+      {:then docs}
+        <Search
+          bind:value={$search}
+          placeholder="Filter previous {type}s"
+          containerClass={"m-4"}
+        />
+        <LinkNavOptions options={generateOption(docs, $search)} />
+      {/await}
+    </LinkNavShell>
   </svelte:fragment>
 </SidebarShell>
--- src/routes/(sidebar)/article/+layout.svelte
+++ src/routes/(sidebar)/article/+layout.svelte
@@ -1,58 +1,23 @@
 <script lang="ts">
-  import SidebarShell from "$com/sidebar/compontents/shell.svelte";
-  import AppSwitcher from "$com/sidebar/compontents/appSwitcher.svelte";
-
-  import LinkNavShell from "$com/sidebar/compontents/linkNav/shell.svelte";
-  import LinkNavOptions from "$com/sidebar/compontents/linkNav/optionList.svelte";
-
-  import type { SidebarOption } from "$shared/types/internal";
-
   import { feedLocalSearch } from "$state/state";
   import { searchInArticle } from "$lib/common/filter";
   import { page } from "$app/stores";
 
-  import type { ArticleBase } from "$shared/types/api";
-  import Loader from "$com/loader.svelte";
-  import Search from "$com/utils/inputs/search.svelte";
+  import Sidebar from "$com/sidebar/linkList.svelte";
 
   $: readArticles = $page.data.readArticles;
-
-  function generateOption(
-    articles: ArticleBase[],
-    search: string
-  ): SidebarOption {
-    return {
-      id: "articles",
-      list: Object.values(articles)
-        .filter((a) => searchInArticle(a, search))
-        .map((article) => ({
-          href: `/article/${article.id}`,
-          label: article.title,
-        })),
-    };
-  }
 </script>
 
-<SidebarShell>
-  <svelte:fragment slot="nav">
-    <AppSwitcher />
-  </svelte:fragment>
-
-  <svelte:fragment slot="content">
-    <LinkNavShell>
-      {#await $readArticles}
-        <Loader text="Loading previous articles" />
-      {:then articles}
-        <Search
-          bind:value={$feedLocalSearch}
-          placeholder={"Filter previous articles"}
-          containerClass={"m-4"}
-        />
-        <LinkNavOptions options={generateOption(articles, $feedLocalSearch)} />
-      {/await}
-    </LinkNavShell>
-  </svelte:fragment>
-</SidebarShell>
+<Sidebar
+  docs={$readArticles}
+  generateOptionLink={(a) => ({
+    href: `/article/${a.id}`,
+    label: a.title,
+  })}
+  searchFilter={(a, search) => searchInArticle(a, search)}
+  search={feedLocalSearch}
+  type="article"
+/>
 
 <main
   class="
