215aa7de970db29630e93dea3c5155fdedbee3ac
bertmad3400 <skrivtilbertram@gmail.com>
2024-06-08 14:54:26 +0000

    Also show collection button on title layout, even with no collections
    See b59fc8865df0

--- src/lib/components/itemList/articles/layouts/title/icons.svelte
+++ src/lib/components/itemList/articles/layouts/title/icons.svelte
@@ -13,35 +13,30 @@
   export let article: ArticleBase;
 
   $: userCollections = $page.data.userCollections;
-  $: collectionCount = Object.values($userCollections).length;
 </script>
 
-{#if (article.similar && article.similar.length > 0) || collectionCount > 0}
-  {#if article.similar && article.similar.length > 0}
-    <button
-      on:click={() => dispatch("showSimilar", article.id)}
-      class="btn px-2"
-      title="Show similar articles"
-    >
-      <Fa
-        icon={faCircleNodes}
-        class="
-        ml-auto my-auto
-        text-lg md:text-xl
-        text-black/75 dark:text-white/90
-        hover:text-primary-500
-        transition-colors
-      "
-      />
-    </button>
-  {/if}
-  {#if collectionCount}
-    <CollectionButton
-      {article}
-      {userCollections}
-      iconClass="text-xl"
-      btnClass="btn aspect-square h-full"
-      overlayClass="top-10 right-0"
+{#if article.similar && article.similar.length > 0}
+  <button
+    on:click={() => dispatch("showSimilar", article.id)}
+    class="btn px-2"
+    title="Show similar articles"
+  >
+    <Fa
+      icon={faCircleNodes}
+      class="
+      ml-auto my-auto
+      text-lg md:text-xl
+      text-black/75 dark:text-white/90
+      hover:text-primary-500
+      transition-colors
+    "
     />
-  {/if}
+  </button>
 {/if}
+<CollectionButton
+  {article}
+  {userCollections}
+  iconClass="text-xl"
+  btnClass="btn aspect-square h-full"
+  overlayClass="top-10 right-0"
+/>
