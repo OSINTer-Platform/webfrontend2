8cd77dab55564777eafbce583cdfc3ff9a1a9c72
bertmad3400 <skrivtilbertram@gmail.com>
2024-07-04 09:45:04 +0000

    Rework types for doc detail list

--- src/lib/components/itemList/header/types.ts
+++ src/lib/components/itemList/header/types.ts
@@ -2,3 +2,13 @@ export interface HeaderListItem {
   highlights?: { [key: string]: string[] };
   summary?: string;
 }
+
+export type VisualDetail =
+  | string
+  | Array<string | { content: string; href: string }>;
+
+export type VisualDetailBatch = {
+  title: string;
+  content: null | undefined | VisualDetail;
+  mono?: boolean;
+};
