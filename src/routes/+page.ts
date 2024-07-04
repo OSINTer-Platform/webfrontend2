8692d9f73d1d140e7e8dd08076bdccd53e71f4a1
bertmad3400 <skrivtilbertram@gmail.com>
2024-07-04 09:45:20 +0000

    Query front-page metrics from API

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,36 +1,25 @@
-import { getTags } from "$lib/common/elasticsearch/aggregations";
-import { searchArticles } from "$lib/common/elasticsearch/search";
-import { shuffle } from "$lib/common/sort";
-
 import type { TrendingArticleBundle } from "$com/frontpage/types";
 import type { PageLoad } from "./$types";
+import type { CVEBase, ClusterBase } from "$shared/types/api";
+
+import { PUBLIC_API_BASE } from "$env/static/public";
+import { error } from "@sveltejs/kit";
 
 export const load: PageLoad = async () => {
-  const oneMonthAgo = new Date(new Date().setMonth(new Date().getMonth() - 1));
-  const tags = await getTags(oneMonthAgo, new Date(), true, [], 4);
+  const r = await fetch(`${PUBLIC_API_BASE}/frontpage`);
 
-  const trendingArticles: TrendingArticleBundle[] = await Promise.all(
-    tags.tags.buckets.map(async ({ key, doc_count, bg_count }) => ({
-      tag: key,
-      count: doc_count + bg_count,
-      articles: shuffle(
-        await searchArticles(
-          {
-            limit: 6,
-            sort_by: "",
-            sort_order: "desc",
-            search_term: key,
-            highlight: true,
-            first_date: oneMonthAgo.toISOString(),
-          },
-          { include_fields: ["title", "url", "image_url"] }
-        )
-      ),
-    }))
-  );
+  if (!r.ok) error(r.status, "Error when getting frontpage metrics");
+
+  const docs: {
+    articles: TrendingArticleBundle[];
+    cves: CVEBase[];
+    clusters: ClusterBase[];
+  } = await r.json();
 
   return {
-    trendingArticles,
+    trendingArticles: docs.articles,
+    trendingCVEs: docs.cves,
+    trendingClusters: docs.clusters,
     burgerMenu: false,
     topbar: false,
   };
